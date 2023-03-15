0473c49f8f7e0ddfdd2f707221cdb3d02faee986
Bartek <voke-ist-ein-dreckiger-huan@gmx.de>
2023-03-15 22:03:38 +0100

    lolo

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { BrowserRouter } from "react-router-dom";
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
-  return (
-    <BrowserRouter basename={window.location.pathname || ''}>
-      <Route exact path="/" component={Index} />
+  return (/* basename={window.location.pathname || ''} */
+    <BrowserRouter >
+      {/* <Route exact path="/" component={Index} /> */}
       <div className='relative z-0 bg-primary'>
         <div className='	bg-cover bg-no-repeat bg-center'>
           <Navbar />
